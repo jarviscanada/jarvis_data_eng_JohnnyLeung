b626eab35ebc74f32be522a35a46e3263aba7164
Johnny Leung <eternaldragonx@hotmail.com>
2021-11-17 17:36:59 -0500

    Switched 2 imports

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -3,9 +3,9 @@ package ca.jrvs.apps.grep;
 import java.io.*;
 import java.util.*;
 import java.util.regex.Pattern;
+import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.apache.log4j.BasicConfigurator;
 
 public class JavaGrepImp implements JavaGrep {
 
