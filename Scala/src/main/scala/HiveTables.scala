3e524ff5ba166d7d65d4f278555ec60c59ba7a6f
Johnny Leung <eternaldragonx@hotmail.com>
2021-12-01 22:58:46 -0500

    Update HiveTables.scala

--- Scala/src/main/scala/HiveTables.scala
+++ Scala/src/main/scala/HiveTables.scala
@@ -21,34 +21,34 @@ object HiveTables extends App {
     import spark.implicits._
     import spark.sql
 
-    sql(s"DROP TABLE IF EXISTS $src")
+    sql(s"DROP TABLE IF EXISTS src")
     
-    sql(s"DROP TABLE IF EXISTS $backup")
+    sql(s"DROP TABLE IF EXISTS backup")
 
     // create the tables
-    sql(s"""CREATE TABLE $src (foo String, bar long NOT NULL, baz BigDecimal)
+    sql(s"""CREATE TABLE src (foo String, bar long NOT NULL, baz BigDecimal)
     PARITIONED BY (part String)""".stripMargin)
       
-    sql(s"""CREATE TABLE $backup (foo String, bar long NOT NULL, baz BigDecimal)
+    sql(s"""CREATE TABLE backup (foo String, bar long NOT NULL, baz BigDecimal)
     PARITIONED BY (part String)""".stripMargin)
     
     sql("set hive.exec.dynamic.partition=true")
     sql("set hive.exec.dynamic.partition.mode=nonstrict")
 
-    sql(s"""LOAD DATA LOCAL INPATH "${args(0)}" INTO TABLE $src""")
+    sql(s"""LOAD DATA LOCAL INPATH "${args(0)}" INTO TABLE src""")
   
-    sql(s"""LOAD DATA LOCAL INPATH "${args(1)}" INTO TABLE $backup""")
+    sql(s"""LOAD DATA LOCAL INPATH "${args(1)}" INTO TABLE backup""")
   
-    val srcDF = sql("SELECT * FROM $src")
-    val backupDF = sql("SELECT * FROM $backup")
+    val srcDF = sql("SELECT * FROM src")
+    val backupDF = sql("SELECT * FROM backup")
   
     // update backup table with new data and overwriting old data
-    val updateDF = sql("SELECT $srcDF .* from $srcDF join $backupDF on $srcDF.part = $backupDF.part")
-    val totalDF = $srcDF.except($updateDF).union($backupDF)
+    val updateDF = sql("SELECT srcDF .* from srcDF join backupDF on srcDF.part = backupDF.part")
+    val totalDF = srcDF.except(updateDF).union(backupDF)
     totalDF.write.mode(SaveMode.Overwrite).saveAsTable("tempTable")
   	
     // write to backup table
-    sql(s"INSERT OVERWRITE $backup PARTITION (part) SELECT * FROM $tempTable")
+    sql(s"INSERT OVERWRITE backup PARTITION (part) SELECT * FROM tempTable")
 
     sparkContext.stop()
   }
