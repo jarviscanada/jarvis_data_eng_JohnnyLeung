8ee92bfbc43c0bae404f91d3f6f8952ad77f4ee8
Johnny Leung <eternaldragonx@hotmail.com>
2021-12-01 22:58:49 -0500

    Update Test.scala

--- Scala/src/test/scala/Test.scala
+++ Scala/src/test/scala/Test.scala
@@ -16,35 +16,35 @@ class Test {
     import spark.sql
   
     // src table
-    sql(s"DROP TABLE IF EXISTS $src")
+    sql(s"DROP TABLE IF EXISTS src")
   
-    sql(s"""CREATE TABLE $src (foo String, bar long NOT NULL, baz BigDecimal)
+    sql(s"""CREATE TABLE src (foo String, bar long NOT NULL, baz BigDecimal)
     PARITIONED BY (part String)""".stripMargin)
   
     sql("set hive.exec.dynamic.partition=true")
     sql("set hive.exec.dynamic.partition.mode=nonstrict")
   
-    sql(s"INSERT INTO $src VALUES('sample1 text', 1, 1.01, 'sample1')")
-    sql(s"INSERT INTO $src VALUES('sample2 text', 2, 2.02, 'sample2')")
-    sql(s"INSERT INTO $src VALUES('sample3 text', 3, 3.03, 'sample3')")
+    sql(s"INSERT INTO src VALUES('sample1 text', 1, 1.01, 'sample1')")
+    sql(s"INSERT INTO src VALUES('sample2 text', 2, 2.02, 'sample2')")
+    sql(s"INSERT INTO src VALUES('sample3 text', 3, 3.03, 'sample3')")
   
-    sql(s"SELECT * from $src").show()
+    sql(s"SELECT * from src").show()
   
     // backup table
-    sql(s"DROP TABLE IF EXISTS $backup")
+    sql(s"DROP TABLE IF EXISTS backup")
   
-    sql(s"""CREATE TABLE $backup (foo String, bar long NOT NULL, baz BigDecimal)
+    sql(s"""CREATE TABLE backup (foo String, bar long NOT NULL, baz BigDecimal)
     PARITIONED BY (part String)""".stripMargin)
   
     sql("set hive.exec.dynamic.partition=true")
     sql("set hive.exec.dynamic.partition.mode=nonstrict")
     
-    sql(s"INSERT INTO $backup VALUES('sample4 text', 4, 4.04, 'sample1')")
-    sql(s"INSERT INTO $backup VALUES('sample5 text', 5, 5.05, 'sample2')")
-    sql(s"INSERT INTO $backup VALUES('sample6 text', 6, 6.06, 'sample3')")
+    sql(s"INSERT INTO backup VALUES('sample4 text', 4, 4.04, 'sample1')")
+    sql(s"INSERT INTO backup VALUES('sample5 text', 5, 5.05, 'sample2')")
+    sql(s"INSERT INTO backup VALUES('sample6 text', 6, 6.06, 'sample3')")
   
-    sql(s"INSERT OVERWRITE $backup PARTITION (part) SELECT * FROM $src")
+    sql(s"INSERT OVERWRITE backup PARTITION (part) SELECT * FROM $src")
   
-    sql(s"SELECT * FROM $backup").show()
+    sql(s"SELECT * FROM backup").show()
   }
 }
