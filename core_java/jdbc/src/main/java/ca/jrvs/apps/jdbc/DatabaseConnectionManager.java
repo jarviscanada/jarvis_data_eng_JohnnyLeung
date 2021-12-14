58baf6936ee3c7f47afb20afdedca155264b732f
Johnny Leung <eternaldragonx@hotmail.com>
2021-12-14 18:52:24 -0500

    Update DatabaseConnectionManager.java

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/DatabaseConnectionManager.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/DatabaseConnectionManager.java
@@ -11,13 +11,13 @@ public class DatabaseConnectionManager {
     private final Properties properties;
 
     public DatabaseConnectionManager(String host, String databaseName, String username, String password) {
-    	this.url = "jdbc:postgresql://" + host + "/" + databaseName;
-    	this.properties = new Properties();
-    	this.properties.setProperty("user", username);
-    	this.properties.setProperty("password", password);
+      this.url = "jdbc:postgresql://" + host + "/" + databaseName;
+      this.properties = new Properties();
+      this.properties.setProperty("user", username);
+      this.properties.setProperty("password", password);
     }
 
     public Connection getConnection() throws SQLException {
-    	return DriverManager.getConnection(this.url, this.properties);
+      return DriverManager.getConnection(this.url, this.properties);
     }
 }
