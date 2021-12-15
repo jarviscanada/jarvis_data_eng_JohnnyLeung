735801e329330432c7b8fd97b0cdc35ebeea65ff
Johnny Leung <eternaldragonx@hotmail.com>
2021-12-15 16:57:27 -0500

    Update LamdaStreamExcImp.java

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/LamdaStreamExcImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/LamdaStreamExcImp.java
@@ -7,190 +7,71 @@ import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-public class LamdaStreamExcImp implements LamdaStreamExc {
+public class LambdaStreamExcImp implements LambdaStreamExc {
 
-  /**
-   * Create a String stream from array
-   *
-   * note: arbitrary number of value will be stored in an array
-   *
-   * @param strings
-   * @return
-   */
-  @Override
-  public Stream<String> createStrStream(String ... strings) {
-	return Stream.of(strings);  
-  }
+    @Override
+    public Stream<String> createStrStream(String ... strings) {
+        return Stream.of(strings);
+    }
 
-  /**
-   * Convert all strings to uppercase
-   * please use createStrStream
-   *
-   * @param strings
-   * @return
-   */
-  @Override
-  public Stream<String> toUpperCase(String ... strings) {
-	return Stream.of(strings).map(s -> s.toUpperCase());
-  }
+    @Override
+    public Stream<String> toUpperCase(String... strings) {
+        return createStrStream(strings).map(element -> element.toUpperCase());
+    }
 
-  /**
-   * filter strings that contains the pattern
-   * e.g.
-   * filter(stringStream, "a") will return another stream which no element contains a
-   *
-   *
-   * @param stringStream
-   * @param pattern
-   * @return
-   */
-  @Override
-  public Stream<String> filter(Stream<String> stringStream, String pattern) {
-	return stringStream.filter(s -> !s.contains(pattern));
-  }
+    @Override
+    public Stream<String> filter(Stream<String> stringStream, String pattern) {
+        return stringStream.filter(string -> string.contains(pattern));
+    }
 
-  /**
-   * Create a intStream from a arr[]
-   * @param arr
-   * @return
-   */
-  @Override
-  public IntStream createIntStream(int[] arr) {
-	return IntStream.of(arr);
-  }
+    @Override
+    public IntStream createIntStream(int[] arr) {
+        return IntStream.of(arr);
+    }
 
-  /**
-   * Convert a stream to list
-   *
-   * @param stream
-   * @param <E>
-   * @return
-   */
-  @Override
-  public <E> List<E> toList(Stream<E> stream) {
-	return stream.collect(Collectors.toList());
-  }
+    @Override
+    public <E> List<E> toList(Stream<E> stream) {
+        return stream.collect(Collectors.toList());
+    }
 
-  /**
-   * Convert a intStream to list
-   * @param intStream
-   * @return
-   */
-  @Override
-  public List<Integer> toList(IntStream intStream) {
-	return intStream.boxed().collect(Collectors.toList());
-  }
+    @Override
+    public List<Integer> toList(IntStream intStream) {
+        return intStream.boxed().collect(Collectors.toList());
+    }
 
-  /**
-   * Create a IntStream range from start to end inclusive
-   * @param start
-   * @param end
-   * @return
-   */
-  @Override
-  public IntStream createIntStream(int start, int end) {
-	return IntStream.range(start, end + 1);
-  }
+    @Override
+    public IntStream createIntStream(int start, int end) {
+        return IntStream.range(start, end);
+    }
 
-  /**
-   * Convert a intStream to a doubleStream
-   * and compute square root of each element
-   * @param intStream
-   * @return
-   */
-  @Override
-  public DoubleStream squareRootIntStream(IntStream intStream) {
-	return intStream.mapToDouble(i -> Math.sqrt(i));
-  }
+    @Override
+    public DoubleStream squareRootIntStream(IntStream intStream) {
+        return intStream.mapToDouble(number -> Math.sqrt(number));
+    }
 
-  /**
-   * filter all even number and return odd numbers from a intStream
-   * @param intStream
-   * @return
-   */
-  @Override
-  public IntStream getOdd(IntStream intStream) {
-	return intStream.filter(i -> i % 2 == 1);
-  }
+    @Override
+    public IntStream getOdd(IntStream intStream) {
+        return intStream.filter(number -> number%2==1);
+    }
 
-  /**
-   * Return a lambda function that print a message with a prefix and suffix
-   * This lambda can be useful to format logs
-   *
-   * You will learn:
-   *   - functional interface http://bit.ly/2pTXRwM & http://bit.ly/33onFig
-   *   - lambda syntax
-   *
-   * e.g.
-   * LambdaStreamExc lse = new LambdaStreamImp();
-   * Consumer<String> printer = lse.getLambdaPrinter("start>", "<end");
-   * printer.accept("Message body");
-   *
-   * sout:
-   * start>Message body<end
-   *
-   * @param prefix prefix str
-   * @param suffix suffix str
-   * @return
-   */
-  @Override
-  public Consumer<String> getLambdaPrinter(String prefix, String suffix) {
-	return message -> System.out.println(prefix + message + suffix);
-  }
+    @Override
+    public Consumer<String> getLambdaPrinter(String prefix, String suffix) {
+        return (String message) -> System.out.println(prefix + message + suffix);
+    }
 
-  /**
-   * Print each message with a given printer
-   * Please use `getLambdaPrinter` method
-   *
-   * e.g.
-   * String[] messages = {"a", "b", "c"};
-   * lse.printMessages(messages, lse.getLambdaPrinter("msg:", "!") );
-   *
-   * sout:
-   * msg:a!
-   * msg:b!
-   * msg:c!
-   *
-   * @param messages
-   * @param printer
-   */
-  @Override
-  public void printMessages(String[] messages, Consumer<String> printer) {
-	for (String message : messages) {
-	  printer.accept(message);
-	}
-  }
+    @Override
+    public void printMessages(String[] messages, Consumer<String> printer) {
+        createStrStream(messages).forEach(message -> printer.accept(message));
+    }
 
-  /**
-   * Print all odd number from a intStream.
-   * Please use `createIntStream` and `getLambdaPrinter` methods
-   *
-   * e.g.
-   * lse.printOdd(lse.createIntStream(0, 5), lse.getLambdaPrinter("odd number:", "!"));
-   *
-   * sout:
-   * odd number:1!
-   * odd number:3!
-   * odd number:5!
-   *
-   * @param intStream
-   * @param printer
-   */
-  @Override
-  public void printOdd(IntStream intStream, Consumer<String> printer) {
-	getOdd(intStream).mapToObj(i -> Integer.toString(i)).forEach(i -> printer.accept(i));
-  }
+    @Override
+    public void printOdd(IntStream intStream, Consumer<String> printer) {
+        getOdd(intStream).mapToObj(number -> Integer.toString(number)).forEach(number -> printer.accept(number));
+    }
 
-  /**
-   * Square each number from the input.
-   * Please write two solutions and compare difference
-   *   - using flatMap
-   *
-   * @param ints
-   * @return
-   */
-  @Override
-  public Stream<Integer> flatNestedInt(Stream<List<Integer>> ints) {
-	return ints.flatMap(i -> i.stream().map(streamInt -> streamInt * streamInt));
-  }
+    @Override
+    public Stream<Integer> flatNestedInt(Stream<List<Integer>> ints) {
+        return ints.flatMap(number -> number.stream().map(x -> x * x));
+    }
+
 }
