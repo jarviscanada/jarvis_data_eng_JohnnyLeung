a632d3b316218c868ea95eea536f285df77e89d5
Johnny Leung <eternaldragonx@hotmail.com>
2021-12-29 16:46:48 -0500

    Update host_info.sh

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 #Setup and validate arguments
 psql_host=$1
 psql_port=$2
@@ -11,3 +12,27 @@ if [ $# -ne 5 ]; then
   echo 'host_info requires 5 arguments'
   exit 1
 fi
+
+#Save CPU architecture information to a variable
+lscpu_out=`lscpu`
+
+#Retrieve hardware specification variables
+hostname=$(hostname -f)
+cpu_number=$(echo "$lscpu_out" | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_out" | egrep "^Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out" | egrep "^Model name:" | awk '{$1="";$2="";print}' | xargs)
+cpu_mhz=$(echo "$lscpu_out" | egrep "^CPU MHz:" | awk '{print $3}' | xargs)
+L2_cache=$(echo "$lscpu_out"  | egrep "(L2)\s(cache)" | awk '{print $3}' | sed 's/.$//' | xargs)
+total_mem=$(cat /proc/meminfo | egrep "MemTotal:" | awk '{print $2}' | xargs)
+
+#Current time in `2019-11-26 14:40:19` UTC format
+timestamp=$(vmstat -t | awk '{print $18" "$19}' | tail -n1 | xargs)
+
+#PSQL command: Inserts hardware specification values into host_info table
+insert_stmt="INSERT INTO host_info(hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, L2_cache, total_mem, timestamp) VALUES('$hostname', '$cpu_number', '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$L2_cache', '$total_mem', '$timestamp');"
+
+#set up env var for pql cmd
+export PGPASSWORD=$psql_password
+#Insert date into a database
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+exit $?
